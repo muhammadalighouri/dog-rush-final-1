1c8a9910039b141e2ce1668cd1514b8305470d6b
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2022-11-09 23:02:09 +0500

    button added

--- src/components/Whitepaper.js
+++ src/components/Whitepaper.js
@@ -7,7 +7,7 @@ const Whitepaper = () => {
                     Contract fully audited by Solid Proof and shown to be 100% secure. Team fully verified by CoinSniper to ensure anti-rug and complete project security.
                 </p>
                 <div className='btn__container'>
-                    <button>Audit</button>
+                    <button onClick={() => window.open('https://app.solidproof.io/projects/dogerush', '_blank')}>Audit</button>
                     <button> Whitepaper</button>
                     <button>KYC</button>
                 </div>
